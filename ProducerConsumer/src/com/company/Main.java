9e47bb30ede9aea0fa156e4bba836f2c27e07808
Bill Sheng <billxsheng@gmail.com>
2020-04-27 19:56:04 -0400

    Java Concurrent Utils

--- ProducerConsumer/src/com/company/Main.java
+++ ProducerConsumer/src/com/company/Main.java
@@ -3,31 +3,69 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.company.Main.EOF;
+
+//  synchronisation drawbacks
+//  1. threads that are blocked waiting to execute synchronised code cannot be interrupted
+//  2. synchronised block must be within the same method
+//  3. cannot test to see if an objects intrinsic lock is available, if it is not available, you cannot timeout after you have waited for the lock for a while
+//  4. If multiple threads are waiting for a lock, it is not first come first serve
+
+//  thread pool- managed set of threads that reduces overhead of thread creation
 
 public class Main {
 
     public static final String EOF = "EOF";
 
     public static void main(String[] args) {
-	    List<String> buffer = new ArrayList<String>();
-	    MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_GREEN);
-	    MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE);
-	    MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN);
+        ArrayBlockingQueue<String> buffer = new ArrayBlockingQueue<String>(6);
 
-	    new Thread(producer).start();
-	    new Thread(consumer1).start();
-	    new Thread(consumer2).start();
+//        if a thread is already holding an reentrant lock and it reaches an object that uses the same lock, it can continue executing
+//        ReentrantLock bufferLock = new ReentrantLock();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_GREEN);
+        MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE);
+        MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN);
+
+//        new Thread(producer).start();
+//        new Thread(consumer1).start();
+//        new Thread(consumer2).start();
+
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
+
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColor.ANSI_BLACK + "I'm being printed from the callable class.");
+                return "This is the callable result.";
+            }
+        });
+
+        try {
+            System.out.println(future.get());
+        } catch (ExecutionException e) {
+            System.out.println("Something went wrong.");
+        } catch (InterruptedException e) {
+            System.out.println("Thread was interrupted.");
+        }
+
+        executorService.shutdown();
     }
 
 }
 
 class MyProducer implements Runnable {
-    private List<String> buffer;
+    private ArrayBlockingQueue<String> buffer;
     private String color;
 
-    public MyProducer(List<String> buffer, String color) {
+    public MyProducer(ArrayBlockingQueue<String> buffer, String color) {
         this.buffer = buffer;
         this.color = color;
     }
@@ -36,11 +74,10 @@ class MyProducer implements Runnable {
         Random random = new Random();
         String[] nums = {"1", "2", "3", "4", "5"};
 
-        for(String num: nums) {
+        for (String num : nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                buffer.add(num);
-
+                buffer.put(num);
                 Thread.sleep(random.nextInt(1000));
             } catch (InterruptedException e) {
                 System.out.println("Producer was interrupted");
@@ -48,30 +85,40 @@ class MyProducer implements Runnable {
         }
 
         System.out.println(color + "Adding EOF and exiting...");
-        buffer.add("EOF");
+        try {
+            buffer.put("EOF");
+        } catch (InterruptedException e) {
+        }
     }
 }
 
 class MyConsumer implements Runnable {
-    private List<String> buffer;
+    private ArrayBlockingQueue<String> buffer;
     private String color;
 
-    public MyConsumer(List<String> buffer, String color) {
+    public MyConsumer(ArrayBlockingQueue<String> buffer, String color) {
         this.buffer = buffer;
         this.color = color;
     }
 
     public void run() {
-        while(true) {
-            if(buffer.isEmpty()) {
-                continue;
-            }
 
-            if(buffer.get(0).equals(EOF)) {
-                System.out.println(color + "Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
+        while (true) {
+//            returns true if lock is successful
+            synchronized (buffer) {
+                try {
+                    if (buffer.isEmpty()) {
+                        continue;
+                    }
+
+                    if (buffer.peek().equals(EOF)) {
+                        System.out.println(color + "Exiting");
+                        break;
+                    } else {
+                        System.out.println(color + "Removed " + buffer.take());
+                    }
+                } catch (InterruptedException e) {
+                }
             }
         }
     }
